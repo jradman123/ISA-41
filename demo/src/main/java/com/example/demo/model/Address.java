14fa2aea60721515bfae238405df42b3be8e4932
Vedrana5 <mijatovic.ra58.2018@uns.ac.rs>
2022-03-23 21:18:34 +0100

    table-adrress

--- demo/src/main/java/com/example/demo/model/Address.java
+++ demo/src/main/java/com/example/demo/model/Address.java
@@ -1,41 +1,76 @@
 package com.example.demo.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="address")
 public class Address {
 
-	private String streetName;
-	private String streetNumber;
-	private City city;
+	 @Id
+	 @Column(name = "id")
+	 @GeneratedValue(strategy = GenerationType.IDENTITY)
+	 private Long id;
+	
+	 @Column(name="streeme", nullable = false)
+	 private String streetName;
+	 @Column(name="streetNumber", nullable = false)
+	 private String streetNumber;
+	 @Column(name="city", nullable = false)
+	 private String city;
+	 @Column(name="country", nullable = false)
+     private String country;
 	
 	public Address() {
 		super();
 	}
-	public Address(String streetName, String streetNumber, City city) {
-		super();
-		this.streetName = streetName;
-		this.streetNumber = streetNumber;
-		this.city = city;
-	}
+
 	
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
 	public String getStreetName() {
 		return streetName;
 	}
+
 	public void setStreetName(String streetName) {
 		this.streetName = streetName;
 	}
+
 	public String getStreetNumber() {
 		return streetNumber;
 	}
+
 	public void setStreetNumber(String streetNumber) {
 		this.streetNumber = streetNumber;
 	}
-	public City getCity() {
+
+	public String getCity() {
 		return city;
 	}
-	public void setCity(City city) {
+
+	public void setCity(String city) {
 		this.city = city;
 	}
-	
-	
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
 	
 	
 }

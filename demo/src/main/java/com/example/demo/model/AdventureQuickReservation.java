1f1a9bb50fbc5e0858fbb8e5258f835c8efa9728
Vedrana5 <mijatovic.ra58.2018@uns.ac.rs>
2022-03-24 19:02:11 +0100

    utility-base

---
+++ demo/src/main/java/com/example/demo/model/AdventureQuickReservation.java
@@ -0,0 +1,47 @@
+package com.example.demo.model;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "adventureQuickReservation")
+@PrimaryKeyJoinColumn(name = "id")
+public class AdventureQuickReservation  extends QuickReservation{
+	 
+	  @ManyToMany
+	    @JoinTable(
+	            name = "adventure_quick_reservation_utilities",
+	            joinColumns = @JoinColumn(name = "adventure_reservation_id"),
+	            inverseJoinColumns = @JoinColumn(name = "adventure_utility_id"))
+	    private Set<QuickReservationUtility> utilities;
+
+   
+	
+   @ManyToOne
+   @JoinColumn(name = "adventure_id")
+   private Adventure adventure;
+   
+   
+ /*  @ManyToOne
+   @JoinColumn(name = "user_id")
+   private RegisteredClient reservationClient;*/
+   
+   
+
+  
+}
+   
+  
+

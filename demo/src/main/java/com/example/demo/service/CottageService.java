acc982c00f217df8d62b296a7b64397936ba3938
Vedrana5 <mijatovic.ra58.2018@uns.ac.rs>
2022-05-10 14:16:11 +0200

    fix

--- demo/src/main/java/com/example/demo/service/CottageService.java
+++ demo/src/main/java/com/example/demo/service/CottageService.java
@@ -2,9 +2,11 @@ package com.example.demo.service;
 
 
 import com.example.demo.dto.CottageDto;
+import com.example.demo.model.cottages.Cottage;
 
 import java.util.List;
 
 public interface CottageService {
     public List<CottageDto> findAll();
+    public Cottage findCottage(Long id);
 }

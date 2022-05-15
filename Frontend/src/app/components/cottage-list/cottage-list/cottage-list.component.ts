fa606d23d8f54ff65b7105e454aefa46a0bf87b7
Vedrana5 <mijatovic.ra58.2018@uns.ac.rs>
2022-05-15 16:00:34 +0200

    Revert "feat:list cottages"
    This reverts commit f621fbd0f178b658706e49091f51e62df5eba92b.

--- Frontend/src/app/components/cottage-list/cottage-list/cottage-list.component.ts
+++ Frontend/src/app/components/cottage-list/cottage-list/cottage-list.component.ts
@@ -1,12 +1,7 @@
+import { Component, Input, OnInit } from '@angular/core';
 import { CottageDto } from 'src/app/interfaces/cottage-list-view';
 import { CottageService } from 'src/app/services/CottageService/cottage.service';
 import { StringLiteralLike } from 'typescript';
-import { Subscription } from 'rxjs';
-import { ChangeDetectorRef, Component, OnInit, ɵɵqueryRefresh } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
-
-import { MatTableDataSource } from '@angular/material/table';
-
 @Component({
   selector: 'app-cottage-list',
   templateUrl: './cottage-list.component.html',
@@ -14,37 +9,20 @@ import { MatTableDataSource } from '@angular/material/table';
 })
 export class CottageListComponent implements OnInit {
 
-  cottages!: MatTableDataSource<CottageDto>;
-  sub!: Subscription;
-  columnsToDisplayRegistrationRequests: string[] = [
-    'No.',
-    'Name',
-    'Description',
-    'Address',
-    'Price',
-    'NumberOfPeople',
-    'Buttons'
-    
-  ];
-  constructor(private cottageService:CottageService,private changeDetectorRefs: ChangeDetectorRef,private _snackBar: MatSnackBar) {
-
-    this.cottages = new MatTableDataSource<CottageDto>();
-  
-   }
+  @Input() cottageOwner: any;
+  cottages: CottageDto[] = [];
+  cottagesAll: CottageDto[] = [];
+  email!: string | null;
+  constructor(private cottageService:CottageService) { }
  
 
   ngOnInit(): void {
-    this.load();
+    this.email = localStorage.getItem('email'); 
+    console.log(this.email);
+    this.cottageService.findByEmail().subscribe((data) => {
+      this.cottages = data;
+      this.cottagesAll = data;
+    });
   }
 
-  load() {
-    this.sub = this.cottageService
-      .findByEmail()
-      .subscribe({
-        next: (cottages: CottageDto[]) => {
-          this.cottages.data = cottages;
-          this.changeDetectorRefs.detectChanges();
-        },
-      });
-    }
 }

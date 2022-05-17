5db395fe58cbc0ac457195bf3d6102f2d8a25445
Jelena Radman <73402841+jradman12@users.noreply.github.com>
2022-05-17 12:52:27 +0200

    removed unused methods in repository

--- demo/src/main/java/com/example/demo/repository/RegistrationRequestRepository.java
+++ demo/src/main/java/com/example/demo/repository/RegistrationRequestRepository.java
@@ -10,13 +10,4 @@ import java.util.List;
 @Repository
 public interface RegistrationRequestRepository extends JpaRepository<RegistrationRequest, Long> {
     RegistrationRequest findByEmail(String email);
-
-    @Query(value = "select * from registration_request r where r.request_status = 1", nativeQuery = true)
-    List<RegistrationRequest> findAllAllowed();
-
-    @Query(value = "select * from registration_request r where r.request_status = 2", nativeQuery = true)
-    List<RegistrationRequest> findAllNotAllowed();
-
-    @Query(value = "select * from registration_request r where r.request_status = 0", nativeQuery = true)
-    List<RegistrationRequest> findAllPending();
 }

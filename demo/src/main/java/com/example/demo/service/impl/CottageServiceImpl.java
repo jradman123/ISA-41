974b9dcdb799d37dc2db443f60f1a15da7fde18e
Vedrana5 <mijatovic.ra58.2018@uns.ac.rs>
2022-05-19 17:25:28 +0200

    fix

--- demo/src/main/java/com/example/demo/service/impl/CottageServiceImpl.java
+++ demo/src/main/java/com/example/demo/service/impl/CottageServiceImpl.java
@@ -93,7 +93,7 @@ public class CottageServiceImpl implements CottageService {
     @Override
     public CottageDto editCottage(CottageDto cottageDto) {
         for (Cottage cottage : cottageRepository.findAll()) {
-            if (cottageDto.getId() == cottage.getId().toString()) {
+            if (cottageDto.getId().equals(cottage.getId().toString())) {
                 cottage.setName(cottageDto.getName());
                 cottage.setDescription(cottageDto.getDescription());
                 cottage.setPrice(Double.parseDouble(cottageDto.getPrice()));

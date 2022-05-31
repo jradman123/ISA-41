4b085aa48e497be609c3d212a95888aa62fbf20e
Vedrana5 <mijatovic.ra58.2018@uns.ac.rs>
2022-05-31 10:37:09 +0200

    availability

--- Frontend/src/app/components/cottage-availability/cottage-availability/cottage-availability.component.ts
+++ Frontend/src/app/components/cottage-availability/cottage-availability/cottage-availability.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { DatePipe } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CottageAvailability } from 'src/app/interfaces/cottage-availability';
 import { AvailabilityService } from 'src/app/services/availabilityService/availability.service';
+import { CottageDto } from 'src/app/interfaces/cottage-list-view';
+import { CottageService } from 'src/app/services/CottageService/cottage.service';
 
 @Component({
   selector: 'app-cottage-availability',
@@ -12,13 +14,19 @@ import { AvailabilityService } from 'src/app/services/availabilityService/availa
 export class CottageAvailabilityComponent implements OnInit {
 
   id: any;
+  cottage!: CottageDto;
+  startDate: any = null;
+  endDate: any = null;
   pipe = new DatePipe('en-US');
   availabilities: CottageAvailability[] = [];
-  constructor(private availabilityService: AvailabilityService, private route: ActivatedRoute) { }
+  constructor(private availabilityService: AvailabilityService, private route: ActivatedRoute, private cottageService: CottageService, private router: Router) { }
 
   ngOnInit(): void {
     this.id = +this.route.snapshot.paramMap.get('id')!;
-    console.log("dsdsfds" + this.id)
+
+    this.cottageService.findbyId(this.id).subscribe((data) => {
+      this.cottage = data;
+    });
 
     this.availabilityService.findAvailabilityByCottage(this.id).subscribe((data) => {
       this.availabilities = data;
@@ -27,4 +35,9 @@ export class CottageAvailabilityComponent implements OnInit {
 
   }
 
+  clickHome() {
+    this.router.navigate(['cottageOwner/cottage-profile/' + this.id]);
+
+
+  }
 }

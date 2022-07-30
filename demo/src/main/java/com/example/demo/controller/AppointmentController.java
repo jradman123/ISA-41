119699e21045fee9bf69d50b4fd9764bd7f28973
Vedrana5 <mijatovic.ra58.2018@uns.ac.rs>
2022-07-30 20:22:03 +0200

    pokusaj

--- demo/src/main/java/com/example/demo/controller/AppointmentController.java
+++ demo/src/main/java/com/example/demo/controller/AppointmentController.java
@@ -1,13 +1,17 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.AppointmentDto;
+import com.example.demo.dto.CottageDto;
+import com.example.demo.dto.QuickReservationDto;
+import com.example.demo.dto.ShipDto;
+import com.example.demo.model.adventures.QuickReservationUtility;
+import com.example.demo.model.reservation.Appointment;
 import com.example.demo.service.impl.AppointmentServiceImpl;
+import com.example.demo.service.impl.ShipServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,8 +24,28 @@ public class AppointmentController {
     private AppointmentServiceImpl appointmentService;
 
 
-    @GetMapping(value="/getAppByCottage/{id}")
+
+    @GetMapping(value="/getApp/{id}")
+    //idCottage or idBoat
     public List<AppointmentDto> getAllByCottage(@PathVariable Long id) {
-       return this.appointmentService.findAppByCottage(id);
+       return this.appointmentService.findApp(id);
     }
+
+    @DeleteMapping(value="/deleteApp/{id}")
+    public ResponseEntity<Long> deleteApp(@PathVariable Long id) {
+        return this.appointmentService.deleteApp(id);
+    }
+
+    @GetMapping(value = "/search")
+    public List<AppointmentDto> search(AppointmentDto dto) {
+        return this.appointmentService.search(dto);
+    }
+
+
+    @RequestMapping(value="/createApp", method = RequestMethod.POST)
+
+    public Appointment createApp(@RequestBody AppointmentDto dto) {
+        return this.appointmentService.createAppointment(dto);
+    }
+
 }
--- demo/src/main/java/com/example/demo/service/AppointmentService.java
+++ demo/src/main/java/com/example/demo/service/AppointmentService.java
@@ -1,10 +1,21 @@
 package com.example.demo.service;
 
 import com.example.demo.dto.AppointmentDto;
+import com.example.demo.dto.QuickReservationDto;
+import com.example.demo.dto.ShipDto;
+import com.example.demo.model.reservation.Appointment;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 public interface AppointmentService {
 
-    List<AppointmentDto> findAppByCottage(Long id);
+    List<AppointmentDto> findApp(Long id);
+
+
+    ResponseEntity<Long> deleteApp(Long id);
+
+    List<AppointmentDto> search(AppointmentDto dto);
+
+   Appointment createAppointment(AppointmentDto dto);
 }

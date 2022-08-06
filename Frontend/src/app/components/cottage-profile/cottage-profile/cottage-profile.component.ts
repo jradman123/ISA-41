851a58c4feffac10bba3bc83e6b7960bde414fbf
Vedrana5 <mijatovic.ra58.2018@uns.ac.rs>
2022-08-06 13:01:26 +0200

    add reservation to form

--- Frontend/src/app/components/cottage-profile/cottage-profile/cottage-profile.component.ts
+++ Frontend/src/app/components/cottage-profile/cottage-profile/cottage-profile.component.ts
@@ -13,6 +13,13 @@ import { ImageService } from 'src/app/services/ImageService/image.service';
 import { ImageDto } from 'src/app/interfaces/image-dto';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { DialogForReservationCottageComponent } from '../../dialog-for-reservation-cottage/dialog-for-reservation-cottage.component';
+import { PersonalData } from 'src/app/interfaces/personal-data';
+import { CottageReservation } from 'src/app/interfaces/cottage-reservation';
+import { UserService } from 'src/app/services/UserService/user.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ReservationService } from 'src/app/services/ReservationService/reservation.service';
+import { Subscription } from 'rxjs';
+
 
 
 export interface DataForDialogCottage {
@@ -27,7 +34,7 @@ export interface DataForDialogCottage {
 export class CottageProfileComponent implements OnInit {
 
 
-
+  fullPrice: number = 0;
   rulee!: RuleDto
   cottage!: CottageDto;
   id: any;
@@ -36,14 +43,24 @@ export class CottageProfileComponent implements OnInit {
   rooms: RoomDto[] = [];
   images: ImageDto[] = [];
   utilityy!: UtilityDto;
+  users!: PersonalData[];
+  newReservation!: CottageReservation
+  form!: FormGroup;
+  formData!: FormData;
+  sub!: Subscription;
 
-  constructor(private route: Router, private cottageService: CottageService, private imageService: ImageService, private router: ActivatedRoute, private ruleService: RuleService, public dialog: MatDialog, private utilityService: UtilityService) {
+  constructor(private route: Router, private reservationService: ReservationService, private userService: UserService, private cottageService: CottageService, private imageService: ImageService, private router: ActivatedRoute, private ruleService: RuleService, public dialog: MatDialog, private utilityService: UtilityService) {
     this.rulee = {} as RuleDto;
     this.utilityy = {} as UtilityDto;
+    this.newReservation = {} as CottageReservation;
   }
 
   ngOnInit(): void {
     this.id = +this.router.snapshot.paramMap.get('id')!;
+
+    this.userService.findAll().subscribe((data) => {
+      this.users = data;
+    });
     this.cottageService.findbyId(this.id).subscribe((data) => {
       this.cottage = data;
     });
@@ -68,7 +85,12 @@ export class CottageProfileComponent implements OnInit {
     });
 
 
-
+    this.form = new FormGroup({
+      clientEmail: new FormControl('', Validators.required),
+      resStart: new FormControl('', Validators.required),
+      resEnd: new FormControl('', Validators.required),
+      numberOfPerson: new FormControl('', Validators.required)
+    })
 
   }
 
@@ -158,6 +180,42 @@ export class CottageProfileComponent implements OnInit {
       });
 
 
+
+  }
+
+  reserved(): void {
+    this.reservateCottage();
+    this.sub = this.reservationService.reservatedCottage(this.newReservation)
+      .subscribe({
+        next: () => {
+
+          window.location.reload();
+
+        }
+      });
+  }
+
+  onNoClick(): void {
+    window.location.reload();
+
+  }
+
+  reservateCottage(): void {
+
+
+    let newStart = new Date(this.form.value.resStart)
+    let newEnd = new Date(this.form.value.resEnd)
+
+
+    console.log(newStart)
+    console.log(newEnd)
+    this.newReservation.resStart = new Date(newStart.setHours(14, 0, 0, 0)),
+      this.newReservation.resEnd = new Date(newEnd.setHours(11, 0, 0, 0)),
+      this.newReservation.numberOfPerson = this.form.value.numberOfPerson;
+    this.newReservation.price = this.cottage.price;
+    this.newReservation.clientEmail = this.form.value.clientEmail;
+    this.newReservation.objectId = this.id;
+    this.newReservation.typeOfRes = 'COTTAGE';
 
   }
 

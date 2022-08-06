dbf29edc1a5e369ea748191f39fdd0036dcddf15
Jelena Radman <73402841+jradman12@users.noreply.github.com>
2022-08-06 17:04:38 +0200

    basic info about adventure

--- Frontend/src/app/components/adventure-profile/adventure-profile.component.ts
+++ Frontend/src/app/components/adventure-profile/adventure-profile.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { DomSanitizer } from '@angular/platform-browser';
 import { ActivatedRoute} from '@angular/router';
 import { AdventureDto } from 'src/app/interfaces/adventure-dto';
 import { Image } from 'src/app/interfaces/image';
 import { ImagesResponse } from 'src/app/interfaces/images-response';
 import { AdventureService } from 'src/app/services/AdventureService/adventure.service';
+import { EditCottageComponent } from '../edit-cottage/edit-cottage/edit-cottage.component';
 
 @Component({
   selector: 'app-adventure-profile',
@@ -20,6 +22,8 @@ export class AdventureProfileComponent implements OnInit {
   image : Image;
   imagesResponse : ImagesResponse;
   images : Image[];
+  initialDetails: any
+  editMode = false
 
   constructor(private adventureService : AdventureService,private router: ActivatedRoute,private _sanitizer: DomSanitizer,) { 
     this.adventure = {}as AdventureDto;
@@ -30,8 +34,20 @@ export class AdventureProfileComponent implements OnInit {
 
   ngOnInit(): void {
     this.id = +this.router.snapshot.paramMap.get('id')!;
-    this.adventureService.findById(this.id).subscribe((data) => {
-      this.adventure = data;
+    this.adventureService.findById(this.id).subscribe({
+      next: (data: AdventureDto) => {
+        this.adventure = data
+        this.initialDetails = JSON.parse(JSON.stringify(data)); 
+        this.detailsForm.controls['name'].setValue(data.name)
+        this.detailsForm.controls['description'].setValue(data.description)
+        this.detailsForm.controls['price'].setValue(data.price)
+        this.detailsForm.controls['cancellationConditions'].setValue(data.cancellationConditions)
+        this.detailsForm.controls['streetName'].setValue(data.streetName)
+        this.detailsForm.controls['streetNumber'].setValue(data.streetNumber)
+        this.detailsForm.controls['city'].setValue(data.city)
+        this.detailsForm.controls['country'].setValue(data.country)
+        this.detailsForm.controls['guestLimit'].setValue(data.guestLimit)
+      },
     });
 
     this.getImages();
@@ -77,4 +93,42 @@ export class AdventureProfileComponent implements OnInit {
     });
   }
 
+  detailsForm = new FormGroup({ 
+    name: new FormControl('', [
+    Validators.required,
+    Validators.pattern('^[A-ZŠĐŽČĆ][a-zšđćčžA-ZŠĐŽČĆ ]*$'),
+  ]),
+  description: new FormControl('', [
+    Validators.required,
+    Validators.pattern('^[A-ZŠĐŽČĆ][a-zšđćčžA-ZŠĐŽČĆ ]*$'),
+  ]),
+  streetName: new FormControl(null, [
+    Validators.required,
+    Validators.pattern('^[A-ZŠĐŽČĆ][a-zšđćčžA-ZŠĐŽČĆ ]*$'),
+  ]),
+  streetNumber: new FormControl(null, [
+    Validators.required,
+    Validators.pattern('^\\d{1,3}$'),
+  ]),
+  city: new FormControl(null, [
+    Validators.required,
+    Validators.pattern('^[A-ZŠĐŽČĆ][a-zšđćčžA-ZŠĐŽČĆ ]*$'),
+  ]),
+  country: new FormControl(null, [
+    Validators.required,
+    Validators.pattern('^[A-ZŠĐŽČĆ][a-zšđćčžA-ZŠĐŽČĆ ]*$'),
+  ]),
+  price: new FormControl(null, [Validators.required]),
+  cancellationConditions: new FormControl(null, [Validators.required]),
+  guestLimit: new FormControl(null, [Validators.required])
+})
+
+
+edit() {
+
+}
+
+cancel(){
+
+}
 }
